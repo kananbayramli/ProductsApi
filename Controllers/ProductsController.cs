822a432a0a53687076a81beec47b95e8f32674ff
kananbayramli <bayramli-kenan@mail.ru>
2024-02-22 13:09:43 +0400

    Models

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,27 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Models;
 
-namespace ProductsAPI;
+namespace ProductsAPI.Controllers;
 
 [ApiController]
 [Route("api/{controller}")]
 public class ProductsController : ControllerBase
 {
-    private static readonly  string[] Products = {
-        "IPhone 14", "IPhone 13 Pro", "IPhone 15"
-    };
+    private static List<Product>? _products;
 
+    public ProductsController()
+    {
+        _products = new List<Product>
+        {
+            new() { ProductId = 1, ProductName = "IPhone 11 Pro Max", Price = 1500, IsActive = true },
+            new() { ProductId = 2, ProductName = "IPhone 12 Pro Max", Price = 2000, IsActive = true },
+            new() { ProductId = 3, ProductName = "IPhone 13 ", Price = 2500, IsActive = true },
+            new() { ProductId = 4, ProductName = "IPhone 14 Pro", Price = 3500, IsActive = true }
+        };
+    }
 
     [HttpGet]
-    public string[] GetProducts()
+    public List<Product> GetProducts()
     {
-        return Products;
+        return _products ?? new List<Product>();
     }
 
     //[HttpGet("api/{controller}/{id}")]
     [HttpGet("{id}")]
-    public string GetProduct( int id )
+    public Product GetProduct( int id )
     {
-        return Products[id];
+        return _products?.FirstOrDefault( i => i.ProductId == id) ?? new Product();
     }
 
 }
